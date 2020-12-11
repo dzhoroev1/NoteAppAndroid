cb91118c632b1a2a77889edc82d997dc8832904d
dzhoroev1gmail.com <dzhoroev1@gmail.com>
2020-12-11 16:41:23 +0900

    [FIX] User -> firebase database saving

--- app/src/main/java/com/example/noteapp/User.java
+++ app/src/main/java/com/example/noteapp/User.java
@@ -1,8 +1,14 @@
 package com.example.noteapp;
 
 public class User {
+    public String name, email;
 
+    public User(){
+
+    }
 
     public User(String name, String email) {
+        this.name = name;
+        this.email = email;
     }
 }
