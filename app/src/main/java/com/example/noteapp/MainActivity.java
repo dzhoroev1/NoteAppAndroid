fdd85b14f1dbd68832afdf7f6e7592fc3387c2be
dzhoroev1gmail.com <dzhoroev1@gmail.com>
2020-12-11 16:32:13 +0900

    [FirebaseAuth implemented] now it can register users.

--- app/src/main/java/com/example/noteapp/MainActivity.java
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.noteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signUp = (Button) findViewById(R.id.registrationButton);
+        signUp.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.registrationButton:
+                startActivity(new Intent(this,RegisterUser.class));
+                break;
+
+        }
     }
 }
--- app/src/main/java/com/example/noteapp/RegisterUser.java
+++ app/src/main/java/com/example/noteapp/RegisterUser.java
@@ -1,14 +1,111 @@
 package com.example.noteapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegisterUser extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterUser extends AppCompatActivity implements View.OnClickListener {
+
+    private Button registerButton,homeButton;
+    private FirebaseAuth mAuth;
+    private EditText fullName, registerEmail, registerPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        registerButton = (Button) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(this);
+
+        homeButton = (Button) findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(this);
+
+        fullName = (EditText) findViewById(R.id.registrationName);
+        registerEmail = (EditText) findViewById(R.id.registrationEmail);
+        registerPassword = (EditText) findViewById(R.id.registrationPassword);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case (R.id.homeButton):
+                startActivity(new Intent(this,MainActivity.class));
+                break;
+            case (R.id.registerButton):
+                registerUser();
+                break;
+        }
+    }
+    private void registerUser(){
+        String email = registerEmail.getText().toString().trim();
+        String name = fullName.getText().toString().trim();
+        String password = registerPassword.getText().toString().trim();
+
+        if (email.isEmpty()){
+            registerEmail.setError("Email is required!");
+            registerEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            registerEmail.setError("Please provide valid email!");
+            registerEmail.requestFocus();
+            return;
+        }
+        if(name.isEmpty()){
+            fullName.setError("Full Name is required!");
+            fullName.requestFocus();
+            return;
+        }
+        if (password.isEmpty()){
+            registerPassword.setError("Password is required!");
+            registerPassword.requestFocus();
+            return;
+        }
+        if (password.length()<6){
+            registerPassword.setError("Too short!");
+            registerPassword.requestFocus();
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            User user = new User(name,email);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()){
+                                        Toast.makeText(RegisterUser.this,"User has been registered syccessfully!",Toast.LENGTH_LONG).show();;
+                                    }else
+                                        Toast.makeText(RegisterUser.this, "Failed to register!",Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        }else{
+                            Toast.makeText(RegisterUser.this, "Failed to register!",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
