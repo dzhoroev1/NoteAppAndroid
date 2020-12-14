6f10c343e9f5346fd994f71c265d3a3325cc68c6
dzhoroev1gmail.com <dzhoroev1@gmail.com>
2020-12-14 15:46:52 +0900

    [ADD] Log out implemented

--- app/src/main/java/com/example/noteapp/ProfileActivity.java
+++ app/src/main/java/com/example/noteapp/ProfileActivity.java
@@ -2,13 +2,34 @@ package com.example.noteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ProfileActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener  {
+    private Button logOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        logOut = (Button) findViewById(R.id.signOutButton);
+
+        logOut.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.signOutButton:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
+
     }
 }
