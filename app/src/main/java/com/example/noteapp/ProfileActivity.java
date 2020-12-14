b2583ac0b0a3138f30f16c875e8a7c1c2da0c82a
dzhoroev1gmail.com <dzhoroev1@gmail.com>
2020-12-14 16:09:19 +0900

    [ADD] shows user name on ProfileActivity

--- app/src/main/java/com/example/noteapp/ProfileActivity.java
+++ app/src/main/java/com/example/noteapp/ProfileActivity.java
@@ -1,25 +1,55 @@
 package com.example.noteapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener  {
     private Button logOut;
-
+    private TextView userInformation;
+    private FirebaseUser user;
+    private DatabaseReference userData;
+    private String userID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
         logOut = (Button) findViewById(R.id.signOutButton);
+        userInformation = (TextView) findViewById(R.id.userInfo);
 
         logOut.setOnClickListener(this);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        userData = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        userData.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+                String name = userProfile.name;
+                userInformation.setText("Welcome, " + name);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
